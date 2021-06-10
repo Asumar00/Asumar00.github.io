6d5227ca9edc40e02bafb538b8983d6cca8be0d4
Asumar00 <d.hayapping.hopping@gmail.com>
2021-06-10 15:02:23 +0900

    bugfix

--- works/counter.js
+++ works/counter.js
@@ -5,7 +5,7 @@ const countUp = () => {
   countElement.innerText = '回数: '+ count
 }
 const resetCount = () => {
-  count--
+  count=0
   const countElement = document.querySelector('#count')
   countElement.innerText = '回数: 0'
 }
